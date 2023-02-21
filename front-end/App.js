8aa06ddafcc0741e50906f46b45f46f92690ba62
Noah Lewis <47840925+TheAssembler1@users.noreply.github.com>
2023-02-20 19:27:07 -0600

    started login page

--- front-end/App.js
+++ front-end/App.js
@@ -1,11 +1,28 @@
 import './controllers/auth/initFirebase';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Pressable, View, TextInput, Text } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+        style={styles.textInput}
+        placeholder='Email'
+        autoComplete='email'
+        autoCorrect={false}
+        keyboardType='email-address'>
+      </TextInput>
+      <TextInput
+        style={styles.textInput}
+        placeholder='Password'
+        secureTextEntry={true}
+        autoCorrect={false}
+        keyboardType='default'>
+      </TextInput>
+      <Pressable
+        style={styles.pressable}>
+          <Text style={styles.text}>Login</Text>
+      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +35,31 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textInput: {
+    borderWidth: 1,
+    minWidth: 210,
+    maxWidth: 210,
+    minHeight: 60,
+    maxHeight: 60,
+    padding: 10,
+    margin: 5,
+    borderRadius: 10,
+    fontSize: 18,
+  },
+  pressable: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 1,
+    minWidth: 210,
+    maxWidth: 210,
+    minHeight: 60,
+    maxHeight: 60,
+    padding: 10,
+    margin: 20,
+    fontSize: 18,
+    borderRadius: 10,
+  },
+  text: {
+    fontSize: 18,
+  }
 });

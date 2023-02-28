e2365151be4e9579166385aca1ac569d6df18c3f
Ruiqi Wang <rwang26@lsu.edu>
2023-02-27 19:26:52 -0600

    update ChooseRole

--- front-end/UserAuth/chooseRole.js
+++ front-end/UserAuth/chooseRole.js
@@ -2,97 +2,62 @@ import React from 'react';
 import { StyleSheet, Pressable, View, TextInput, Text, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
-const chooseRole = () => {
-  const navigation = useNavigation();
-  
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Create account</Text>
-      <Text style={styles.createAccount}>
-        Have an account?{' '}
-        <TouchableOpacity onPress={() => navigation.navigate('Login')}>
-          <Text style={styles.link}>Login here</Text>
-        </TouchableOpacity></Text>
-        <TextInput
-        style={styles.textInput}
-        placeholder='Email'
-        autoComplete='email'
-        autoCorrect={false}
-        keyboardType='email-address'
-        backgroundColor='#fbfbfb'
-        width={280}
-        maxLength={40}>
-      </TextInput>
-      <TextInput
-        style={styles.textInput}
-        placeholder='Password'
-        secureTextEntry={true}
-        autoCorrect={false}
-        keyboardType='default'
-        backgroundColor='#fbfbfb'
-        width={280}
-        maxLength={40}>
-      </TextInput>
-      <Pressable
-        style={styles.pressable}>
-          <Text style={styles.Buttontext}>Submit</Text>
-      </Pressable>
-    </View>
-  );
-};
 
-const styles = StyleSheet.create({
+const ChooseRole = () => {
+    const navigation = useNavigation();
+  
+    const handleButtonPress1 = () => {
+        console.log("Student");
+    };
+      
+  
+    const handleButtonPress2 = () => {
+        console.log("Tutor");
+    };
+  
+    return (
+      <View style={styles.container}>
+        <Text style={styles.promptText}>User Account Type</Text>
+        <Pressable style={[styles.button, {backgroundColor: '#3F3F3F80'}]} onPress={handleButtonPress1}>
+        <Text style={[styles.buttonText, {flex: 1, flexWrap: 'wrap'}]}>Student</Text>
+        </Pressable>
+        <Pressable style={[styles.button, {backgroundColor: '#3F3F3F80'}]} onPress={handleButtonPress2}>
+        <Text style={[styles.buttonText, {flex: 1, flexWrap: 'wrap'}]}>Tutor</Text>
+        </Pressable>
+      </View>
+    );
+  };
+  
+  const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
       justifyContent: 'center',
+      alignItems: 'center',
+      flexDirection: 'column',
     },
-    title: {
-      fontSize: 34,
-      fontWeight: 'normal', 
-      marginBottom: 20,
-      paddingRight:61,
+    promptText: {
+      fontSize: 35,
+      fontWeight: 'bold',
+      marginBottom: 10,
     },
-    createAccount: {
-        flexDirection: 'row',
-        alignItems: 'flex-start', 
-        fontSize: 15,
-        lineHeight: 25,
-        marginBottom: 5,
-        color: '#777', 
-        paddingRight:79,
+    buttonContainer: {
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    button: {
+        width: 150,
+        height: 50,
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderRadius: 5,
+        marginVertical: 10,
+        justifyContent: 'center',
       },
-    link: {
-      color: '#42A5F5',
-      fontSize: 15,
-      lineHeight: 31,
-    },
-    textInput: {
-      borderWidth: 1,
-      width: 280,
-      height: 60,
-      padding: 10,
-      margin: 5,
-      borderRadius: 3,
-      fontSize: 18,
-    },
-    pressable: {
-      alignItems: 'center',
-      justifyContent: 'center',
-      borderWidth: 1,
-      width: 280,
-      height: 60,
-      padding: 10,
-      margin: 5,
-      fontSize: 18,
-      borderRadius: 3,
-      backgroundColor: 'gray',
-    },
-    Buttontext: {
-      fontSize: 18,
-      color: 'white',
-    }
+      buttonText: {
+        color: '#FFFFFF',
+        fontSize: 20,
+        textAlign: 'center',
+      },
   });
 
-export default chooseRole;
+export default ChooseRole;

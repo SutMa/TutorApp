c7e5c2bbf84fae692238f2f1b16b84a474090acc
Bryce <boliv31@lsu.edu>
2023-03-15 12:50:30 -0500

    added role to user login

--- front-end/controllers/auth/user.js
+++ front-end/controllers/auth/user.js
@@ -1,21 +1,22 @@
 import { createDoc, createDocById, getDocById, docExists } from '../firebaseCrud';
 const USER_PATH = 'user';
 
-export async function signUp(email, password){
+export async function signUp(email, password, role){
     if(!docExists(USER_PATH, email)){
         throw new Error('Account already exists.');
     }
     await createDocById(USER_PATH, email, {
         password: password,
+        role: role,
     });
 }
 
-export async function signIn(email, password){
+export async function signIn(email, password, role){
     if(!docExists(USER_PATH, email)){
         return false;
     }
     const user = await getDocById(USER_PATH, email);
-    if(user.password != password){
+    if(user.password != password || user.role != role){
         return false;
     }
     return true;

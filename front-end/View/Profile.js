604b5eac44844b1eea0762b81096a0192e02454e
Ruiqi Wang <rwang26@lsu.edu>
2023-03-23 22:53:00 -0500

    update clearUserStorage() and welcome username.

--- front-end/View/Profile.js
+++ front-end/View/Profile.js
@@ -1,39 +1,66 @@
 import { Text } from 'react-native';
 import React from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { logoutUser } from '../controllers/auth/user';
+import { clearUserStorage } from '../controllers/auth/user';
 import { TouchableOpacity, StyleSheet, View  } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
+
+//get user name function
+
+// const [username, setUsername] = useState('');
+
+// const getUsername = async () => {
+//   const storedUsername = await AsyncStorage.getItem('username');
+//   if (storedUsername) {
+//     setUsername(storedUsername);
+//   }
+// };
+
+// useEffect(() => {
+//   getUsername();
+// }, []);
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    button: {
-      backgroundColor: '#d3d3d3',
-      paddingHorizontal: 20,
-      paddingVertical: 10,
-      borderRadius: 5,
-      width: '50%', // the width is 50%
-    },
-    buttonText: {
-      fontSize: 18,
-      color: '#000',
-      textAlign: 'center', // make the button align center
-    },
-  });
-  
-  export default function LoginButton() {
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: '#d3d3d3',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 5,
+    width: '50%',
+    marginTop: 20, // add margin to the top
+  },
+  buttonText: {
+    fontSize: 18,
+    color: '#000',
+    textAlign: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold', // make the text bold
+    color: 'red', // set the text color to red
+    marginBottom: 20, // add margin to the bottom
+  },
+});
+
+
+
+export default function LoginButton() {
     const navigation = useNavigation();
-  
-    const handleLoginNavigation = () => {
+
+    const handleLoginNavigation = async () => {
+      await clearUserStorage(); 
       navigation.navigate('Login');
     };
-  
+
     return (
       <View style={styles.container}>
+        <Text style={styles.text}>Welcome, username!</Text>
         <TouchableOpacity
           style={styles.button}
           onPress={handleLoginNavigation}
@@ -42,4 +69,5 @@ const styles = StyleSheet.create({
         </TouchableOpacity>
       </View>
     );
+
   }
